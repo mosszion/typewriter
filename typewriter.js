6064ca9fd5f6500a30f3479d6fae26c1addace1d
Mussie <musdzion@gmail.com>
2024-03-14 07:15:29 -0400

    comments added

--- typewriter.js
+++ typewriter.js
@@ -1,12 +1,14 @@
-const sentence = "hello lighthouse labs, this is moss learning timeout functions \n";
-let delay = 0;
+const sentence = "hello lighthouse labs, this is moss learning timeout functions \n"; //input string with new line at the end
+let delay = 0;   ///delay starts at 0
+
 // looping through the sentence
+
 for(let letter of sentence){
   setTimeout(() => {
-    process.stdout.write(letter);
+    process.stdout.write(letter);  //prints all letters on same line
 
-  }, delay);
-  delay += 50;
+  }, delay);    //timer guided by delay variable
+  delay += 50;  //delay adds 50ms for every loop cycle
 }
 
 
